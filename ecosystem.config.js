2df165dd7d86001ee498f597d77803ab37dfc015
zhaosaisai <zhaosaisai@u51.com>
2019-05-05 19:47:50 +0800

    first init

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    name: 'upload',
+    script: 'index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    },
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    output: './logs/out.log',
+    error: './logs/error.log'
+  }]
+};
---
+++ index.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const fs = require('fs')
+const Koa = require('koa')
+const koaBody = require('koa-body')
+const router = require('koa-router')()
+const uuid = require('uuid/v1')
+
+const app = new Koa()
+const port = process.env.NODE_PORT || 8989
+const artifact = path.resolve(__dirname, 'artifacts')
+const log = console.log.bind(console)
+
+ensureArtifact(artifact)
+
+app.use(koaBody({
+  multipart: true,
+  formidable: {
+    maxFieldsSize: 10 * 1024 * 1024
+  }
+}))
+
+router.post('/upload', async (ctx) => {
+  if (!ctx.request.files) {
+    ctx.status = 400
+    ctx.body = {
+      code: 1,
+      message: '缺少files参数'
+    }
+    return
+  }
+  const name = uuid()
+  log(`[Create filename] -- ${name}`)
+  const file = ctx.request.files.file
+  log(`[Temp file] -- ${file.path}`)
+  const filename = ctx.request.body.name || '.tar.gz'
+  log(`[Upload filename] -- ${filename}`)
+  const reader = fs.createReadStream(file.path)
+  reader.pipe(fs.createWriteStream(path.join(artifact, `${name}${getFullExtname(filename)}`)))
+  ctx.body = name
+})
+
+app.use(router.routes())
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`)
+})
+
+function ensureArtifact(artifact) {
+  (fs.existsSync(artifact) && fs.statSync(artifact).isDirectory()) || fs.mkdirSync(artifact)
+}
+
+function getFullExtname(filename) {
+  return filename.substr(filename.indexOf('.'))
+}
