27bef50533bbca33dd12cbe2d9129a4d7adc2822
zhaosaisai <zhaosaisai@u51.com>
2019-05-05 20:42:49 +0800

    update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,5 +15,18 @@ module.exports = {
     log_date_format: 'YYYY-MM-DD HH:mm:ss',
     output: './logs/out.log',
     error: './logs/error.log'
-  }]
+  }],
+  deploy: {
+    production: {
+      key: '~/.ssh/id_rsa',
+      user: 'root',
+      host: ['118.24.204.202'],
+      ssh_options: "StrictHostKeyChecking=no",
+      ref: "origin/master",
+      repo: "https://github.com/zhaosaisai/upload.git",
+      path: '/root/projects/upload',
+      'post-setup': "ls -la",
+      'post-deploy': "yarn run prod"
+    }
+  }
 };
